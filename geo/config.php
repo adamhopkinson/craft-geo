62a902ff95c8fdb4f914c466fd7ced21ef8b3485
adamhopkinson <adam.hopkinson@gmail.com>
2018-10-08 19:54:49 +0100

    Add a config variable for the IP API key
    See https://ipapi.com/quickstart

--- geo/config.php
+++ geo/config.php
@@ -6,4 +6,6 @@ return array(
   "defaultIp"=>"190.93.246.7", // Costa Rica
   // "defaultIp"=>"92.111.6.18", Netherland no city or region
   "cacheTime"=>"43200" //seconds
+  // API Key for IPAPI.com - which replaced Nekudo on 2018/10/08
+  "ipApiKey"=>"APIKEYHERE"
 );
